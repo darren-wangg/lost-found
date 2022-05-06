7853892a6a8c2e7c16e1172c9aade2bcd72a80ba
Larry Xu <lhxu@usc.edu>
2022-05-06 10:54:25 -0700

    updated sql file

--- Script.sql
+++ Script.sql
@@ -14,7 +14,7 @@ CREATE TABLE user_profile (
 
 DROP TABLE IF EXISTS post_like;
 CREATE TABLE post_like (
-  id VARCHAR(15) NOT NULL PRIMARY KEY, 
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
   post_id VARCHAR(15) REFERENCES user_post(id),
   profile_email VARCHAR(50) REFERENCES user_profile(email),
   created_time TIMESTAMP
@@ -22,7 +22,7 @@ CREATE TABLE post_like (
 
 DROP TABLE IF EXISTS user_post;
 CREATE TABLE user_post (
-    id VARCHAR(15) NOT NULL PRIMARY KEY, 
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     profile_email VARCHAR(50) REFERENCES user_profile(email),
     written_text TEXT,
     created_datetime TIMESTAMP
