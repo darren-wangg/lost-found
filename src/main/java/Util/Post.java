ce3226f18e94fa430514faba4ab335c38634e42f
Angela Ma <agma@usc.edu>
2022-05-05 20:26:46 -0700

    Like parameters in Post.java

--- src/main/java/Util/Post.java
+++ src/main/java/Util/Post.java
@@ -1,5 +1,7 @@
 package Util;
 
+import java.sql.SQLException;
+
 import Dao.PostDao;
 
 public class Post {
@@ -7,17 +9,19 @@ public class Post {
 	private String profile_email;
 	private String written_text;
 	private String created_datetime;
+	private Integer likes;
 	
-	public Post(String id_, String profile_email_, String written_text_, String created_datetime_) {
+	public Post(String id_, String profile_email_, String written_text_, String created_datetime_, Integer likes_) {
 		id = id_;
 		profile_email = profile_email_;
 		written_text = written_text_;
 		created_datetime = created_datetime_;
+		likes = likes_;
 	}
 	
-	public Post() {
-		
-	}
+	//everytime we add a like, need to update like parameter with PostDao.getlikes() again
+	
+	public Post() {}
 	
 	public String getId() {
 		return id;
@@ -51,5 +55,8 @@ public class Post {
 		created_datetime = cd;
 	}
 	
+	public Integer getLikes() {
+		return likes;
+	}
 	
 }
